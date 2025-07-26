4c8a7f1ab504d480995c640bef7d66de4d8adce9
cole <cole.donat@gmail.com>
2025-07-25 22:49:03 -0400

    Added space for external links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,15 @@ import './Header.css';
 const Header = () => {
   return (
     <div className="app-header">
-      <h1>
-        <span style={{ "color": "rgb(100, 0, 255)" }}>Log</span>
-        <span style={{ "color": "rgb(255, 0, 0)" }}>i</span>
-        <span style={{ "color": "rgb(100, 0, 255)" }}>t Lens</span>
-      </h1>
+      <a className="header-link" href="/about">About</a>
+      <div className="title-container">
+        <h1>
+          <span style={{ "color": "rgb(100, 0, 255)" }}>Log</span>
+          <span style={{ "color": "rgb(255, 0, 0)" }}>i</span>
+          <span style={{ "color": "rgb(100, 0, 255)" }}>t Lens</span>
+        </h1>
+      </div>
+      <a className="header-link" href="/faq">What's happening here?</a>
     </div>
   );
 };
