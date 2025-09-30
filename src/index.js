76d91e115ad1e23644ac1288c4be86a21d695f85
cole <cole.donat@gmail.com>
2025-09-30 14:06:35 -0400

    Updated for gh-pages deploy

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import './App.css';
 import App from './App';
 import About from './pages/About';
 import FAQ from './pages/FAQ';
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router";
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
         <Route path="/faq" element={<FAQ />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
